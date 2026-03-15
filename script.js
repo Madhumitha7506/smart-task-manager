fa89a027fb540533a149dd46c4da5145d6feb4c4
Madhumitha7506 <madhuangappan7506@gmail.com>
2026-03-15 17:15:29 +0530

    Update script.js
    Added local storage feature to save tasks

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function displayTasks(){
+let taskList = document.getElementById("taskList");
+taskList.innerHTML = "";
+
+tasks.forEach((task,index)=>{
+
+let li = document.createElement("li");
+li.innerText = task.text;
+
+if(task.completed){
+li.classList.add("completed");
+}
+
+li.onclick = function(){
+tasks[index].completed = !tasks[index].completed;
+localStorage.setItem("tasks", JSON.stringify(tasks));
+displayTasks();
+};
+
+let deleteBtn = document.createElement("button");
+deleteBtn.innerText = "Delete";
+
+deleteBtn.onclick = function(){
+tasks.splice(index,1);
+localStorage.setItem("tasks", JSON.stringify(tasks));
+displayTasks();
+};
+
+li.appendChild(deleteBtn);
+taskList.appendChild(li);
+
+});
+}
+
 function addTask(){
 
 let input = document.getElementById("taskInput");
@@ -8,24 +44,14 @@ alert("Please enter a task");
 return;
 }
 
-let li = document.createElement("li");
-li.innerText = taskText;
+tasks.push({text:taskText,completed:false});
 
-li.onclick = function(){
-li.classList.toggle("completed");
-};
+localStorage.setItem("tasks", JSON.stringify(tasks));
 
-let deleteBtn = document.createElement("button");
-deleteBtn.innerText = "Delete";
+input.value="";
 
-deleteBtn.onclick = function(){
-li.remove();
-};
-
-li.appendChild(deleteBtn);
-
-document.getElementById("taskList").appendChild(li);
-
-input.value = "";
+displayTasks();
 
 }
+
+displayTasks();
